bc71485db369e740467ce918b596978ba525ea6e
Flowdeeps <pete@republicofpete.net>
2013-01-29 01:02:22 +0100

    Fixed terrible function where main nav functions killed the info tab functionality

--- js/media-player-init.js
+++ js/media-player-init.js
@@ -1,6 +1,5 @@
 $(document).ready( domSetup );
 
-
 function domSetup() {
   // nav
   $('.tabs nav.main a').bind('click',function(e){
@@ -11,11 +10,29 @@ function domSetup() {
       $('.tabs nav.main a').removeClass('active');
       $(this).addClass('active');
       matchRel = $(this).attr('rel');
-      $('.tabs > div').slideUp(function(){
+      $(this).parent().parent().parent().siblings('div').slideUp(function(){
         setTimeout(function(){
           $('.tabs > div#'+ matchRel).slideDown();
         },500);
       });
     }
   });
+
+  // secondary nav
+  $('.tabs nav.secondary a').bind('click',function(e){
+    e.preventDefault();
+    if ($(this).hasClass('active')) {
+      return false;
+    } else {
+      $('.tabs nav.secondary a').removeClass('active');
+      $(this).addClass('active');
+      matchRel = $(this).attr('rel');
+      $(this).parent().parent().parent().siblings('div').slideUp(function(){
+        setTimeout(function(){
+          $('.tabs > div#'+ matchRel).slideDown();
+        },500);
+      });
+    }
+  });
+
 }
